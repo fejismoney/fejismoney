75be2ce7625fb03f0cba77f33a6818b4fa9d7f24
fejiro <85490641+fejismoney@users.noreply.github.com>
2021-06-16 10:45:49 +0100

    Add files via upload

---
+++ work.js
@@ -0,0 +1,14 @@
+// This function takes in one number type as its parameter
+// The function converts convert from Celsius to Fahrenheit.
+// The formular to convert to fahrenheit is "temperature in Celsius times 9 / 5, plus 32".
+
+function convertToFahrenheit(celsius) {
+    
+    return (celsius * (9/5) + 32)
+}
+console.log
+(convertToFahrenheit(-30))
+console.log(convertToFahrenheit(-10))
+
+convertToF(-30) // -> -22
+convertToF(-10) // -> 14
